c83b346dc2005d4bf68a636e53284ff328e1b15c
Katiasun <katjatolok3@gmail.com>
2024-02-17 12:09:10 +0100

    write forEach for buttons

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+//set initial count
 let count = 0;
 
+//select value and buttons
+const value = document.getElementById("value");
+const buttons = document.querySelectorAll(".btn");
 
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (el) => {
+
+    })
+})
