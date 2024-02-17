ee75f7442d1b5455f44032624f75c5372713ab4e
Katiasun <katjatolok3@gmail.com>
2024-02-17 12:29:51 +0100

    write loop

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ const value = document.getElementById("value");
 const buttons = document.querySelectorAll(".btn");
 
 buttons.forEach((btn) => {
-    btn.addEventListener("click", (el) => {
+    btn.addEventListener("click", (e) => {
+        let styles = e.currentTarget.classList;
 
-    })
-})
+        if(styles.contains("decrease")) {
+            count--;
+        }else if(styles.contains("increase")) {
+            count++;
+        }else {
+            count =0;
+        }
+        value.textContent = count;
+    });
+});
